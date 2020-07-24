ebc74c97b268bac5d04f61c4fe35923566b77b88
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-24 16:47:49 +0200

    Added logging for monitoring

--- deregister-sub/deregister-sub.js
+++ deregister-sub/deregister-sub.js
@@ -2,4 +2,8 @@ module.exports.handler = async (event) => {
   const getBulkBody = require('./helpers/get-bulk-body.js')
   const deleteSub = require('./helpers/delete-sub.js')
   return await deleteSub(getBulkBody(event.detail.responsePayload.deadNotifs))
+    .then(esRes => {
+      console.log(JSON.stringify(esRes, null, 2))
+      return esRes
+    })
 }
