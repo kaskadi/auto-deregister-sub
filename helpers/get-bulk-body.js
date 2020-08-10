df9b49baca3320388f0948f667c5c506383aea2f
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-10 16:09:54 +0200

    Updated folder structure

--- helpers/get-bulk-body.js
+++ helpers/get-bulk-body.js
@@ -4,9 +4,9 @@ module.exports = (deadNotifs) => {
 
 function getOpBodyDuplets (user) {
   return deadEndpoint => {
-    const op = { update: { _id: user, _index: 'users'} }
+    const op = { update: { _id: user, _index: 'users' } }
     const body = {
-      script : {
+      script: {
         lang: 'painless',
         source: 'def matchSub; def subs = ctx._source.subscriptions; for (sub in subs) { if (sub.endpoint == params.endpoint) { matchSub = sub; } } subs.remove(subs.indexOf(matchSub))',
         params: {
